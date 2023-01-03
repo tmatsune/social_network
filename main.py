ae29e89a47b8924d0ced0b1786c80747dcaa38b8
terencematsune <matsuneterence@gmail.com>
2023-01-03 01:05:41 -0800

    commit sqlite to postgres

--- main.py
+++ main.py
@@ -86,7 +86,7 @@ bootstrap = Bootstrap(app)
 # connect to SQlite DB
 app.config['SECRET_KEY'] = os.getenv('secret_key')
 og = 'sqlite:///social_network.db'
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get("DATABASE_URL")
+app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
